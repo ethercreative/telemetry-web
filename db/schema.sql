7f13f9a26fa4a9fae37cf37bf200c4695ac93063
Tam <hi@tam.sx>
2020-01-09 12:32:49 +0000

    Add templates / server end points

--- db/schema.sql
+++ db/schema.sql
@@ -8,7 +8,15 @@ create schema if not exists "public";
 
 -- Tables
 
-create table "public"."stats" (
+create table if not exists "public"."plugins" (
+    "id" uuid primary key default (public.uuid_generate_v4()),
+    "handle" text not null,
+    "name" text not null,
+    "developer" text not null,
+    "icon" text not null
+);
+
+create table if not exists "public"."stats" (
     "id" uuid primary key default (public.uuid_generate_v4()),
     "key" uuid not null,
     "handle" text not null,
@@ -22,3 +30,7 @@ create table "public"."stats" (
     "env" text not null,
     "php" text not null
 );
+
+alter table "public"."stats" add column "createdAt" timestamptz not null default (now());
+alter table "public"."stats" alter column "key" type text;
+alter table "public"."stats" alter column "license" drop not null;
--- server.js
+++ server.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 const express = require('express')
 	, nunjucks = require('nunjucks')
 	, postgres = require('postgres')
-	, fs = require('fs');
+	, fs = require('fs')
+	, querystring = require('querystring')
+	, crypto = require('crypto');
 
 const { DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
 
@@ -29,6 +31,9 @@ function manifest () {
 // Routes
 // =========================================================================
 
+// Routes: Index
+// -------------------------------------------------------------------------
+
 app.get('/', async (req, res) => {
 	let data;
 
@@ -44,6 +49,72 @@ app.get('/', async (req, res) => {
 		data,
 		manifest: manifest(),
 	});
+});
+
+// Routes: Plugin
+// -------------------------------------------------------------------------
+
+app.get('/:handle', async (req, res) => {
+	// TODO: Lookup handle in DB
+	const plugin = {
+		handle: req.params.handle,
+		name: req.params.handle.toUpperCase(),
+	};
+
+	// TODO: If plugin doesn't exist, 404
+
+	res.status(200).render('_view.twig', {
+		plugin,
+		manifest: manifest(),
+	});
+});
+
+// Routes: Tell
+// -------------------------------------------------------------------------
+
+app.post('/', async (req, res) => {
+	const data = req.get('X-Telemetry');
+
+	if (data === '') {
+		const key = crypto.randomBytes(128).toString('hex');
+		// TODO: Check key against DB?
+		res.status(200).send(key);
+		return;
+	}
+
+	// End the request immediately to prevent any unnecessary slowness from
+	// our end
+	res.status(200).end();
+
+	const info = querystring.parse(data.replace(/%5B(\d*)%5D/g, ''));
+
+	if (!Array.isArray(info.editions))
+		info.editions = [info.editions];
+
+	const allowedKeys = [
+		'key',
+		'handle',
+		'version',
+		'edition',
+		'editions',
+		'installed',
+		'enabled',
+		'license',
+		'issues',
+		'env',
+		'php',
+	];
+
+	Object.keys(info).forEach(key => {
+		if (allowedKeys.indexOf(key) === -1)
+			delete info[key];
+	});
+
+	info.editions = sql.array(info.editions);
+
+	sql`insert into stats ${sql(info)}`;
+	
+	// TODO: If no plugin exists with the given handle in our DB try to get it
 });
 
 // Listen
